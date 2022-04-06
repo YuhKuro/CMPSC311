8553ff0d8b72958ca8325589bb362889c937f782
ThomasWatson123 <69825559+ThomasWatson123@users.noreply.github.com>
2022-04-05 22:04:59 -0400

    Add files via upload

---
+++ MessengerS.c
@@ -0,0 +1,77 @@
+#include <stdio.h>
+
+#include <sys/socket.h> //For Sockets
+
+#include <stdlib.h>
+
+#include <netinet/in.h> //For the AF_INET (Address Family)
+
+#include <unistd.h>
+
+#include <fcntl.h>
+
+#include <errno.h>
+
+
+
+
+
+struct sockaddr_in serv; // Main Socket Variable
+
+int fd; // File Descriptor for the socket
+
+int conn; //This is the connection file descriptor which will be used to retrieve client connections
+
+char message[100]; // Stores the messages being sent by the server
+
+
+
+int main()
+
+{
+
+    serv.sin_family = AF_INET;
+
+    serv.sin_port = htons(18000); // Defines the port at where the server will listen for connections
+
+    serv.sin_addr.s_addr = INADDR_ANY;
+
+    int flags; // Used for making the socket non blocking
+
+
+
+    fd = socket(AF_INET, SOCK_STREAM, 0); // This creates a new socket and returns the identifier of the socket into fd
+
+
+
+    if(fd < 0) // Checks to see if there was an error creating the socket
+
+        perror("Socket Error");
+
+    bind(fd, (struct sockaddr *)&serv, sizeof(serv)); //Assigns the address specified by serv to the socket
+
+    listen(fd, 5); // Listens for client connections only allows 5
+
+
+
+    while(conn = accept(fd, (struct sockaddr *)NULL, NULL)){
+
+        pid_t pid;
+
+        if((pid = fork()) == 0){
+
+            while (recv(conn, message, 100, 0)>0){
+
+                printf("%s",message);
+
+            }
+
+            exit(0);
+
+        }
+
+    }
+
+    return 0;
+
+}
---
+++ User2.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+
+#include <sys/socket.h>
+
+#include <stdlib.h>
+
+#include <netinet/in.h>
+
+#include <arpa/inet.h>
+
+#include <string.h>
+
+
+
+
+
+struct sockaddr_in serv; // Main Socket Variable
+
+int fd; // File Descriptor for the socket
+
+int conn; //This is the connection file descriptor which will be used to retrieve client connections
+
+char message[100]; // Stores the messages being sent by the server
+
+char header[500] = "User2: "; // Assigns the tag User2 to the message to identify who is sending the message
+
+
+
+int main()
+
+{
+
+    fd = socket(AF_INET, SOCK_STREAM, 0); // Saves the sockets file descriptor to a variable fd
+
+    serv.sin_family = AF_INET; // Designates the address family to internet protocol
+
+    serv.sin_port = htons(18000); // Connects to the sockets port
+
+    inet_pton(AF_INET, "127.0.0.1", &serv.sin_addr); //This binds the client to localhost
+
+    connect(fd, (struct sockaddr *)&serv, sizeof(serv)); //This connects the client to the server.
+
+    while(1) {
+
+        printf("Enter a message: ");
+
+        fgets(message, 100, stdin); // Gets message input from user in console
+
+        strcat(header, message); // Puts User tag infront of message
+
+        send(fd, header, strlen(message) + strlen(header), 0); // Sends message to the socket
+
+    }
+
+}
