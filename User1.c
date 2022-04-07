c132dd07b0bdedfc77a7b1a5b9101c9df50c919b
Lucas <sallada.lucas@gmail.com>
2022-04-07 12:18:57 -0400

    fix: clear User message buffer after sending

--- User1.c
+++ User1.c
@@ -50,6 +50,9 @@ int main()
 
         send(fd, header, strlen(message) + strlen(header), 0); // Sends message to the socket
 
+        memset(&header, 0,  sizeof(message)); // clear message sent buffer
+
+        strcpy(header, "User1: "); // reset to default message
     }
 
 }
